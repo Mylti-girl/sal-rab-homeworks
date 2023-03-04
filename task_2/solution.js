1ea7c052a75dee4d7c56ccf9aa88d7c9bd528c7e
Mylti-girl <55713770+Mylti-girl@users.noreply.github.com>
2023-03-04 22:33:25 +0300

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,6 +16,14 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
+    let shippingSum;
+
+    if(productsSum == 0 || productsSum >= freeShippingMinSum){
+        shippingSum = 0 
+    } else if(productsSum > 0 && productsSum < freeShippingMinSum){
+        shippingSum = shippingPrice
+    };
+
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -34,6 +42,14 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
+    let discountSum;
+
+    if(productsSum >= discountMinSum){
+        discountSum = productsSum * discountPart/100 
+    } else {
+        discountSum = 0
+    };
+
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -49,14 +65,20 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
-
+    
+    let totalSum = productsSum - discountSum;
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
+    totalSum = totalSum + shippingSum;
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+
+    let freeShipping;
+
+    freeShipping = !Boolean(shippingSum);
 
     // Конец решения задачи №2.3.
 
